f970dfbd1ec43fa76bcd721ebad7051f6a833064
mottan <nissy.nao.1001@gmail.com>
2022-07-03 01:19:56 +0900

    初期ファイルです

---
+++ src/test/java/com/workshop/yuiman/YuimanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.yuiman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuimanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
