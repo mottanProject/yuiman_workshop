6b76204fa5d61c7df8fd302566dbb89053f48d70
mottan <nissy.nao.1001@gmail.com>
2022-07-06 00:50:42 +0900

    お試し用にSpringWebをDependenciesに追加 HelloWorldが実行できない＝Mavenインストール必要?

--- src/main/java/com/workshop/yuiman/YuimanApplication.java
+++ src/main/java/com/workshop/yuiman/YuimanApplication.java
@@ -2,9 +2,16 @@ package com.workshop.yuiman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class YuimanApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(YuimanApplication.class, args);
