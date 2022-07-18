ef7305f69fc879ab3e8e0023d0acfcea4694a25f
mottan <nissy.nao.1001@gmail.com>
2022-07-18 15:25:30 +0900

    DB名を変更しスキーマ作成。

--- docker/postgresql/init/init.sql
+++ docker/postgresql/init/init.sql
@@ -1,16 +1,21 @@
 /** テーブル初期定義 **/
+/** DB定義 **/
+CREATE DATABASE workshopdb;
 
-/** postgresに移動 **/
-\c postgres
+/** workshopdbに移動 **/
+\c workshopdb
+
+/** スキーマyumanを追加 **/
+CREATE SCHEMA yuiman;
 
 /**  login_info **/
-CREATE TABLE public.t_login_info (uid SERIAL NOT NULL,
+CREATE TABLE yuiman.t_login_info (uid SERIAL NOT NULL,
                                   mailaddress VARCHAR(355) NOT NULL,
                                   password VARCHAR(50) NOT NULL,
                                   login_date_last TIMESTAMP,
                                   PRIMARY KEY(uid), UNIQUE(mailaddress));
 /** user_info **/
-CREATE TABLE public.t_user_info (uid INTEGER NOT NULL,
+CREATE TABLE yuiman.t_user_info (uid INTEGER NOT NULL,
                                  name VARCHAR(20) NOT NULL,
                                  u_type_id INTEGER NOT NULL,
                                  auth BOOLEAN NOT NULL,
@@ -19,7 +24,7 @@ CREATE TABLE public.t_user_info (uid INTEGER NOT NULL,
                                  profile_picture_path VARCHAR(255),
                                  PRIMARY KEY(uid));
 /** movie_list **/
-CREATE TABLE public.t_movie_list (movie_id SERIAL NOT NULL,
+CREATE TABLE yuiman.t_movie_list (movie_id SERIAL NOT NULL,
                                  title VARCHAR(50) NOT NULL,
                                  m_type_id INTEGER NOT NULL,
                                  uid INTEGER,
@@ -29,10 +34,10 @@ CREATE TABLE public.t_movie_list (movie_id SERIAL NOT NULL,
                                  release_date TIMESTAMP NOT NULL,
                                  PRIMARY KEY(movie_id));
 /** user_type  **/
-CREATE TABLE public.t_user_type (u_type_id INTEGER NOT NULL,
+CREATE TABLE yuiman.t_user_type (u_type_id INTEGER NOT NULL,
                                  u_type_name VARCHAR(20) NOT NULL,
                                  PRIMARY KEY(u_type_id));
 /** movie_type **/
-CREATE TABLE public.t_movie_type (m_type_id INTEGER NOT NULL,
+CREATE TABLE yuiman.t_movie_type (m_type_id INTEGER NOT NULL,
                                   m_type_name VARCHAR(50) NOT NULL,
                                   PRIMARY KEY(m_type_id));
