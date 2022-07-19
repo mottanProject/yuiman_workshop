7c3baeb8a8165b5e0a7fa9e4ea21aab018046d9c
mottan <nissy.nao.1001@gmail.com>
2022-07-20 06:48:40 +0900

    テーブル定義の変更に伴うSQLの修正。 ・ユーザ情報にワークシートURLを追加 ・ムービー情報にアルバムジャンルを追加 ・アルバムジャンル情報テーブルを追加 ※詳細はED書参照

--- docker/postgresql/init/init.sql
+++ docker/postgresql/init/init.sql
@@ -24,12 +24,14 @@ CREATE TABLE yuiman.t_user_info (uid INTEGER NOT NULL,
                                  profile_picture_path VARCHAR(255),
                                  movie_times INTEGER DEFAULT 0,
                                  review_times INTEGER DEFAULT 0,
+                                 sheet_url VARCHAR(255),
                                  PRIMARY KEY(uid));
 /** movie_list **/
 CREATE TABLE yuiman.t_movie_list (movie_id SERIAL NOT NULL,
                                  title VARCHAR(50) NOT NULL,
                                  m_type_id INTEGER NOT NULL,
                                  uid INTEGER,
+                                 album_genre_id INTEGER,
                                  movie_url VARCHAR(255) NOT NULL,
                                  release BOOLEAN NOT NULL,
                                  create_date TIMESTAMP NOT NULL,
@@ -43,3 +45,8 @@ CREATE TABLE yuiman.t_user_type (u_type_id INTEGER NOT NULL,
 CREATE TABLE yuiman.t_movie_type (m_type_id INTEGER NOT NULL,
                                   m_type_name VARCHAR(50) NOT NULL,
                                   PRIMARY KEY(m_type_id));
+
+/** album_genre **/
+CREATE TABLE yuiman.t_album_genre (album_genre_id INTEGER NOT NULL,
+                                  genre_name VARCHAR(50) NOT NULL,
+                                  PRIMARY KEY(album_genre_id));
